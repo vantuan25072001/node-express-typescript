d06f35fedd094a2cde3d2120cb28785dcb2da7d3
vantuan25072001 <vantuan25072001@gmail.com>
2023-11-15 15:48:44 +0700

    vantuan

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import compression from "compression";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.use(
 );
 
 app.use(compression());
-app.use(compression());
+app.use(cookieParser());
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
@@ -22,3 +23,12 @@ const server = http.createServer(app);
 server.listen(8000, () => {
   console.log("Server running on http://localhost:8000/");
 });
+
+const MONGO_URL =
+  "mongodb+srv://vantuan25072001:vantuan25072001@cluster0.779he7u.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.Promise = Promise;
+
+mongoose.connect(MONGO_URL);
+
+mongoose.connection.on("error", (error: Error) => console.log(error));
